383b6bcc2705548b1242adba65544da6ecbcecf8
Tuan Nguyen <tuan.vivaviet@gmail.com>
2021-01-29 06:27:31 +0700

    remove unsed code

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -24,63 +24,6 @@ class Home extends Component {
         this.props.clearUserList();
         this.props.getUserList(text);
     }
-
-    componentDidMount() {
-        const newArr = [];
-        let unavailableItems = [
-            { startPx: 10, endPx: 30 },
-            { startPx: 55, endPx: 65 },
-            { startPx: 35, endPx: 50 },
-            { startPx: 20, endPx: 40 },
-            { startPx: 60, endPx: 70 },
-        ];
-        const sorted = [
-            ...unavailableItems.sort((a, b) => a.startPx - b.startPx),
-        ];
-        let overlapIndex = null;
-        sorted.map((line, index) => {
-            if (index !== overlapIndex) {
-                overlapIndex = sorted.findIndex(
-                    (item) =>
-                        (item.startPx > line.startPx &&
-                            item.startPx < line.endPx) ||
-                        (line.endPx < item.endPx && line.endPx > item.startPx)
-                );
-                if (overlapIndex > -1) {
-                    const newLine = {
-                        startPx: line.startPx,
-                        endPx:
-                            line.endPx > sorted[overlapIndex].endPx
-                                ? line.endPx
-                                : sorted[overlapIndex].endPx,
-                    };
-                    newArr.push(newLine);
-                } else {
-                    newArr.push(line);
-                }
-            }
-            console.log('EEEEE', newArr);
-            console.log('DDDDD', sorted[overlapIndex]);
-
-            return line;
-        });
-
-        // sorted.reduce((acc, curr, currIndex) => {
-        //     console.log('AAAAAAAAAAAA', curr);
-        //     console.log('bbbbbbb', currIndex);
-        //     if (acc && acc.startPx) {
-        //         console.log('ccc', curr);
-        //         unavailableItems.splice(0, 1);
-        //     } else {
-        //     }
-        // });
-        // unavailableItems.map((p, i) => {
-        //     unavailableItems.splice(i, 1);
-
-        //     console.log('aaaaaaaaaaaaa', unavailableItems);
-        //     return p;
-        // });
-    }
 }
 
 const mapStateToProps = (state) => {
